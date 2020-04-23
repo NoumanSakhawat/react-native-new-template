ffcbd9e074386f401ce27d50e242cf07b4286c0d
Nouman Sakhawat <rnouman28@gmail.com>
2020-04-23 12:44:20 +0500

    New Update

--- template/App.js
+++ template/App.js
@@ -1,49 +1,133 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
+import React, { Component } from 'react';
+import { ActivityIndicator, Alert, Image, StyleSheet, Text, TouchableHighlight, View } from 'react-native';
+import { ViroARSceneNavigator } from 'react-viro';
+import renderIf from './js/helpers/renderIf';
 
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+var InitialARScene = require('./js/ARHitTestSample');
 
-type Props = {};
-export default class App extends Component<Props> {
+// Array of 3d models that we use in this sample. This app switches between this these models.
+var objArray = [
+  require('./js/res/coffee_mug/object_coffee_mug.vrx'),
+  require('./js/res/object_flowers/object_flowers.vrx'),
+  require('./js/res/emoji_smile/emoji_smile.vrx')];
+
+export default class ViroSample extends Component {
+  constructor() {
+    super();
+
+    this._onShowObject = this._onShowObject.bind(this);
+    this._renderTrackingText = this._renderTrackingText.bind(this);
+    this._onTrackingInit = this._onTrackingInit.bind(this);
+    this._onDisplayDialog = this._onDisplayDialog.bind(this);
+    this._onLoadStart = this._onLoadStart.bind(this);
+    this._onLoadEnd = this._onLoadEnd.bind(this);
+
+    this.state = {
+      viroAppProps: { displayObject: false, objectSource: objArray[0], yOffset: 0, _onLoadEnd: this._onLoadEnd, _onLoadStart: this._onLoadStart, _onTrackingInit: this._onTrackingInit },
+      trackingInitialized: false,
+      isLoading: false,
+    }
+  }
+
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+      <View style={localStyles.outer} >
+        <ViroARSceneNavigator style={localStyles.arView} apiKey="YOUR API KEY"
+          initialScene={{ scene: InitialARScene, passProps: { displayObject: this.state.displayObject } }} viroAppProps={this.state.viroAppProps}
+        />
+
+        {this._renderTrackingText()}
+
+        {renderIf(this.state.isLoading,
+          <View style={{ position: 'absolute', left: 0, right: 0, top: 0, bottom: 0, alignItems: 'center', justifyContent: 'center' }}>
+            <ActivityIndicator size='large' animating={this.state.isLoading} color='#ffffff' />
+          </View>)
+        }
+
+        <View style={{ position: 'absolute', left: 0, right: 0, bottom: 77, alignItems: 'center' }}>
+          <TouchableHighlight style={localStyles.buttons}
+            onPress={this._onDisplayDialog}
+            underlayColor={'#00000000'} >
+            <Image source={require("./js/res/btn_mode_objects.png")} />
+          </TouchableHighlight>
+        </View>
       </View>
     );
   }
+
+  // Invoked when a model has started to load, we show a loading indictator.
+  _onLoadStart() {
+    this.setState({
+      isLoading: true,
+    });
+  }
+
+  // Invoked when a model has loaded, we hide the loading indictator.
+  _onLoadEnd() {
+    this.setState({
+      isLoading: false,
+    });
+  }
+
+  _renderTrackingText() {
+    if (this.state.trackingInitialized) {
+      return (<View style={{ position: 'absolute', backgroundColor: "#ffffff22", left: 30, right: 30, top: 30, alignItems: 'center' }}>
+        <Text style={{ fontSize: 12, color: "#ffffff" }}>Tracking initialized.</Text>
+      </View>);
+    } else {
+      return (<View style={{ position: 'absolute', backgroundColor: "#ffffff22", left: 30, right: 30, top: 30, alignItems: 'center' }}>
+        <Text style={{ fontSize: 12, color: "#ffffff" }}>Waiting for tracking to initialize.</Text>
+      </View>);
+    }
+  }
+
+  _onTrackingInit() {
+    this.setState({
+      trackingInitialized: true,
+    });
+  }
+
+  _onDisplayDialog() {
+    Alert.alert(
+      'Choose an object',
+      'Select an object to place in the world!',
+      [
+        { text: 'Coffee Mug', onPress: () => this._onShowObject(0, "coffee_mug", 0) },
+        { text: 'Flowers', onPress: () => this._onShowObject(1, "flowers", .290760) },
+        { text: 'Smile Emoji', onPress: () => this._onShowObject(2, "smile_emoji", .497823) },
+      ],
+    );
+  }
+
+  _onShowObject(objIndex, objUniqueName, yOffset) {
+    this.setState({
+      viroAppProps: { ...this.state.viroAppProps, displayObject: true, yOffset: yOffset, displayObjectName: objUniqueName, objectSource: objArray[objIndex] },
+    });
+  }
 }
 
-const styles = StyleSheet.create({
-  container: {
+var localStyles = StyleSheet.create({
+  outer: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+
+  arView: {
+    flex: 1,
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+
+  buttons: {
+    height: 80,
+    width: 80,
+    paddingTop: 20,
+    paddingBottom: 20,
+    marginTop: 10,
+    marginBottom: 10,
+    backgroundColor: '#00000000',
+    borderRadius: 10,
+    borderWidth: 1,
+    borderColor: '#ffffff00',
+  }
 });
+
+module.exports = ViroSample
--- template/index.js
+++ template/index.js
@@ -2,9 +2,13 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
+import { AppRegistry } from 'react-native';
 import App from './App';
 
 const APP_NAME = 'ProjectName';
+const APP_NAMETB = "ProjectName";
 
 AppRegistry.registerComponent(APP_NAME, () => App);
+
+// The below line is necessary for use with the TestBed App
+AppRegistry.registerComponent(APP_NAMETB, () => App);
